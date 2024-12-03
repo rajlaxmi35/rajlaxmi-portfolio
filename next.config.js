dbd75a81dc75540dc16a198af6e09a03b521af59
Rajlaxmi <rajlaxmig27@gmail.com>
2024-12-04 01:30:43 +0530

    Updated package.json and next.config.js for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   basePath: '/rajlaxmi-portfolio',
-  assetPrefix: '/rajlaxmi-portfolio',
+  assetPrefix: '/rajlaxmi-portfolio/',
+  trailingSlash: true, // Ensures all paths end with a slash
 };
 
 module.exports = nextConfig;
